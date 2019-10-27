b400844c74df3f910dee23f49c95db642b6168ca
Jeff Fu <1093928135@qq.com>
2019-10-27 17:50:55 +0800

    basic practice of python and shell script

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print("hello world!")
+'This is a hello world program.'
+
+__author__ = 'Jeff Fu'
+
+import sys
+
+def output():
+	args = sys.argv
+	if len(args) == 1:
+		print 'Hello World!'
+	elif len(args) == 2:
+		print 'Hello', args[1] + '!'
+	else:
+		print 'Too many arguments'
+if __name__ == '__main__':
+	output()
